b47570b25e05b2f456337f90fa86ec5ca589bcfa
kokeb Abera <kokzbest@gmail.com>
2022-11-03 09:19:53 -0500

    Update main.java

--- main.java
+++ main.java
@@ -1,6 +1,6 @@
 
-// class HelloWorld {
-//     public static void main(String[] args) {
-//         System.out.println("Hello, World!");
-//     }
-// }
+ class HelloWorld {
+    public static void main(String[] args) {
+         System.out.println("Hello, World!");
+    }
+ }
