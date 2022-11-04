d569685e920adca7eade942d993638238fc30758
kokeb86 <kokzbest@gmail.com>
2022-11-04 15:15:39 -0500

    add parameter gitHubActionter

--- main.java
+++ main.java
@@ -1,6 +1,8 @@
 
  class HelloWorld {
+    public static String gitHubAction="Wellcome to GitHub Action";
     public static void main(String[] args) {
          System.out.println("Hello, World!");
+        System.out.println("Hello, World!=="+gitHubAction);
     }
  }
